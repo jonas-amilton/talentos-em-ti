f5dd2ec8a1b7e3c0c476c3a042592b5b923ba0b3
jonas <jonas96@edu.unisinos.br>
2023-08-16 10:10:56 -0300

    [new] novo exercicio

--- aula-07-operadores-e-logica-de-programacao/exercicios/index.js
+++ aula-07-operadores-e-logica-de-programacao/exercicios/index.js
@@ -9,3 +9,23 @@ console.log((4-5)*-1);
 
 //determinar o resto da divisao de 234 por 5
 console.log(234%5);
+
+// Antes de começar, crie 3 variáveis: a, b e c. Atribua os
+// valores true, false e true, respectivamente
+// a = true b = false c = true
+
+let souColorado = true; //a
+let tenhoCarro = false; //b
+let tenhoGato = false; //c
+
+// realize a operacao a && b
+console.log(souColorado && tenhoCarro);
+
+// realize a operacao b && c
+console.log(tenhoCarro && tenhoGato);
+
+// realize a operacao a && c
+console.log(souColorado && tenhoGato);
+
+// realize a operacao a && b && c
+console.log(souColorado && tenhoCarro && tenhoGato);
