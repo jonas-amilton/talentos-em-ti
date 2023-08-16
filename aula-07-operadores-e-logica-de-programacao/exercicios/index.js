aeec0735860d18a2c6f8dc77eb86399734a9ca36
jonas <jonas96@edu.unisinos.br>
2023-08-16 10:00:16 -0300

    [new] novos exercicios

---
+++ aula-07-operadores-e-logica-de-programacao/exercicios/index.js
@@ -0,0 +1,11 @@
+//somar 3 com 4
+console.log(3+4)
+
+//multiplicar 3 com 5 e dividir o resultado por 2
+console.log((3*5)/2)
+
+//subtrair 5 de 4 e multiplicar o resultado por -1
+console.log((4-5)*-1);
+
+//determinar o resto da divisao de 234 por 5
+console.log(234%5);
