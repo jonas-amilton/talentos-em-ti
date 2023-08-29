17fd3221b9e7969c52ccf0864fd61bf2af5abef4
jonas <jonas96@edu.unisinos.br>
2023-08-29 09:56:06 -0300

    [new] novo exercicio

---
+++ aula-13/ex-01/index.js
@@ -0,0 +1,24 @@
+let filme = {
+  nome: "The Batman",
+  ano: 2022,
+  listaElenco: [
+    "Robert Pattinson",
+    "Zoe Kravits",
+    "Paul Dano",
+    "Colin Farrel",
+    "Jeffrey Wright",
+    "Andy Serkis",
+    "John Turturro",
+  ],
+  jaViu: true,
+};
+
+console.log(filme.nome);
+console.log(filme.ano);
+console.log(filme.listaElenco);
+console.log(filme.jaViu);
+
+console.log(filme["nome"]);
+console.log(filme["ano"]);
+console.log(filme["listaElenco"]);
+console.log(filme["jaViu"]);
