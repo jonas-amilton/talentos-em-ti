d844495cf410e72e21f9caff33ba886f60d7c7b9
jonas <jonas96@edu.unisinos.br>
2023-08-29 09:58:21 -0300

    [update] incluindo diretor

--- aula-13/ex-01/index.js
+++ aula-13/ex-01/index.js
@@ -1,6 +1,7 @@
 let filme = {
   nome: "The Batman",
   ano: 2022,
+  diretor: "Matt Reeves",
   listaElenco: [
     "Robert Pattinson",
     "Zoe Kravits",
@@ -15,10 +16,12 @@ let filme = {
 
 console.log(filme.nome);
 console.log(filme.ano);
+console.log(filme.diretor);
 console.log(filme.listaElenco);
 console.log(filme.jaViu);
 
 console.log(filme["nome"]);
 console.log(filme["ano"]);
+console.log(filme["diretor"]);
 console.log(filme["listaElenco"]);
 console.log(filme["jaViu"]);
