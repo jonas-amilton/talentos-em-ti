a443d2910ed0fb35a79a76c07393ca82a0d6a6a8
jonas <jonas96@edu.unisinos.br>
2023-08-30 09:55:48 -0300

    [new] ex resolvido

--- aula-14/escrita-2/index.js
+++ aula-14/escrita-2/index.js
@@ -1,34 +1,40 @@
-
 /**
- * 
+ *
  * Resolva os passos a seguir
- * 
- * a) Crie dois "objetos" diferentes com as seguintes propriedades: nome, idade e profissão. 
- * 
- * b) Escreva uma "função" que receba um "objeto" de cada vez e retorne um "array" com as seguintes informações:
+ *
+ * a) Crie dois "objetos" diferentes com as seguintes propriedades: nome, idade e profissï¿½o.
+ *
+ * b) Escreva uma "funï¿½ï¿½o" que receba um "objeto" de cada vez e retorne um "array" com as seguintes informaï¿½ï¿½es:
  * - O valor de "nome".
  * - O numero de caracteres do valor "nome".
  * - O valor de "idade".
- * - O valor de "profissão".
- * - O numero de caracteres do valor "profissão".
- * 
-**/
+ * - O valor de "profissï¿½o".
+ * - O numero de caracteres do valor "profissï¿½o".
+ *
+ **/
 
 const pessoa = {
-	nome: "Bruno", 
-	idade: 23, 
-	profissao: "Instrutor"
-}
-
+  nome: "Bruno",
+  idade: 23,
+  profissao: "Instrutor",
+};
 
 function minhaFuncao(pessoa) {
-	let resultado = []
+  let resultado = [];
 
-	// Desenvolver sua logica
+  // Desenvolver sua logica
 
-	return resultado
+  let qtdCaracteresNome = pessoa.nome.length;
+  let qtdCaracteresProfissao = pessoa.profissao.length;
+
+  resultado.push(pessoa.nome);
+  resultado.push(qtdCaracteresNome);
+  resultado.push(pessoa.idade);
+  resultado.push(pessoa.profissao);
+  resultado.push(qtdCaracteresProfissao);
+
+  return resultado;
 }
 
-
-console.log(minhaFuncao(pessoa))
+console.log(minhaFuncao(pessoa));
 // Retorno: ["Bruno", 5, 23, "Instrutor", 9]
