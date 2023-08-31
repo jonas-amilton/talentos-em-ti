6a175fb08ecef347774a6ed9ff750f16acd08b52
jonas <jonas96@edu.unisinos.br>
2023-08-31 09:30:04 -0300

    [refactor]

--- aula-14/desafio-3/index.js
+++ aula-14/desafio-3/index.js
@@ -2,6 +2,9 @@
 /**
  * 
  * Função extra
- * Crie uma função a mais para o exercício 3 de escrita de código. Essa função vai auxiliar o controle de estoque do sacolão: ela deve receber por parâmetro uma das frutas e retornar esse mesmo objeto com a propriedade disponibilidade com o valor invertido.
+ * Crie uma função a mais para o exercício 3 de escrita de código.
+ * Essa função vai auxiliar o controle de estoque do sacolão:
+ * ela deve receber por parâmetro uma das frutas e retornar
+ * esse mesmo objeto com a propriedade disponibilidade com o valor invertido.
  * 
 **/
