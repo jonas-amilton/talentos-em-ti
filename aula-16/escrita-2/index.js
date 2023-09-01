98b527ffea461c247792c01a2953815519a2115f
jonas <jonas96@edu.unisinos.br>
2023-09-01 10:53:21 -0300

    [new] novos exercicios

---
+++ aula-16/escrita-2/index.js
@@ -0,0 +1,8 @@
+
+/**
+ * 
+ * Faça um programa que verifica que turno do dia um aluno estuda.
+ * 
+ * Peça para digitar "M" (matutino), "V" (Vespertino) ou "N" (Noturno). Imprima no console a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!". Utilize os blocos "if/else if/else".
+ * 
+**/
---
+++ aula-16/escrita-3/index.js
@@ -0,0 +1,8 @@
+
+/**
+ * 
+ * Repita o exercício anterior, mas utilizando a estrutura de "switch/case".
+ * 
+ * Dica: Lembre-se que "switch/case" é como se fosse um "if", mas verifica somente igualdades estritas (===).
+ * 
+**/
