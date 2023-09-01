6d86b7c0a1db83e2ac6d6d14bf9aa29a3f68a69f
jonas <jonas96@edu.unisinos.br>
2023-09-01 09:22:13 -0300

    [new] nova lista de exercicios

---
+++ aula-16/interpretacao-1/index.js
@@ -0,0 +1,18 @@
+
+// Leia o código
+
+const respostaDoUsuario = prompt("Digite o número que você quer testar")
+const numero = Number(respostaDoUsuario)
+
+if (numero % 2 === 0) {
+	console.log("Passou no teste.")
+}
+else {
+	console.log("Não passou no teste.")
+}
+
+// a) Explique o que o código faz. Qual o teste que ele realiza?
+
+// b) Para que tipos de números ele imprime no console "Passou no teste"?
+
+// c) Para que tipos de números a mensagem é "Não passou no teste"?
---
+++ aula-16/interpretacao-3/index.js
@@ -0,0 +1,17 @@
+
+// Leia o código
+
+const numero = Number(prompt("Digite o primeiro número."))
+
+if (numero > 0) {
+	console.log("Esse número passou no teste")
+	let mensagem = "Essa mensagem é secreta!"
+}
+
+console.log(mensagem)
+
+// a) O que a primeira linha está fazendo?
+
+// b) Considere um usuário digitou o número 10. Qual será a mensagem do terminal? E se fosse o número -10?
+
+// c) Haverá algum erro no console? Justifique usando os conceitos de bloco ou escopo.
