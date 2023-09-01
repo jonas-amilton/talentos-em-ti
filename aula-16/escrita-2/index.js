62081f457a83ec7d9f5fbb79c0e31e2a1a9c561a
jonas <jonas96@edu.unisinos.br>
2023-09-01 10:57:51 -0300

    [new] nova solucao

--- aula-16/escrita-2/index.js
+++ aula-16/escrita-2/index.js
@@ -1,8 +1,23 @@
-
 /**
- * 
- * Faça um programa que verifica que turno do dia um aluno estuda.
- * 
- * Peça para digitar "M" (matutino), "V" (Vespertino) ou "N" (Noturno). Imprima no console a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!". Utilize os blocos "if/else if/else".
- * 
-**/
+ *
+ * Faï¿½a um programa que verifica que turno do dia um aluno estuda.
+ *
+ * Peï¿½a para digitar "M" (matutino), "V" (Vespertino) ou "N" (Noturno).
+ * Imprima no console a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!".
+ * Utilize os blocos "if/else if/else".
+ *
+ **/
+
+let turno = prompt(
+  'Digite um turno \n "M" (matutino) \n "V" (Vespertino) \n "N" (Noturno)'
+).toLocaleUpperCase();
+
+if (turno === "M") {
+  alert("Bom Dia!");
+} else if (turno === "V") {
+  alert("Boa Tarde!");
+} else if (turno === "N") {
+  alert("Boa Noite!");
+} else {
+  alert("Turno Invalido!");
+}
