25ee6ccf4cc2ac51e1f19187216101921472763c
jonas <jonas96@edu.unisinos.br>
2023-09-05 09:38:41 -0300

    [new] nova solucao

--- aula-18/interpretacao-2/index.js
+++ aula-18/interpretacao-2/index.js
@@ -1,5 +1,5 @@
 
-// Leia o código abaixo
+// Leia o cï¿½digo abaixo
 
 const lista = [ 10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30 ]
 
@@ -10,5 +10,17 @@ for (let numero of lista) {
 }
 
 // a) O que vai ser impresso no console?
+/**
+ * 19
+ * 21
+ * 23
+ * 25
+ * 27
+ * 30
+ */
 
-// b) Se eu quisesse acessar o índice de cada elemento dessa lista, a sintaxe "for...of..." é suficiente? Se sim, o que poderia ser usado para fazer isso?
+// b) Se eu quisesse acessar o ï¿½ndice de cada elemento
+//    dessa lista, a sintaxe "for...of..." ï¿½ suficiente?
+//    Se sim, o que poderia ser usado para fazer isso?
+
+// teria que usar um for tradicional
