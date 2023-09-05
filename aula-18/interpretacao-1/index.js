58720c844c089ef03d9839efb92079ab18dfa383
jonas <jonas96@edu.unisinos.br>
2023-09-05 09:30:35 -0300

    [new] exercicios interpretacao

---
+++ aula-18/interpretacao-1/index.js
@@ -0,0 +1,13 @@
+
+/**
+ * O que o código abaixo está fazendo?
+ * Qual o resultado impresso no console?
+**/
+
+let valor = 0
+
+for (let i = 0; i < 5; i++) {
+	valor += i
+}
+
+console.log(valor)
---
+++ aula-18/interpretacao-2/index.js
@@ -0,0 +1,14 @@
+
+// Leia o código abaixo
+
+const lista = [ 10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30 ]
+
+for (let numero of lista) {
+	if (numero > 18) {
+		console.log(numero)
+	}
+}
+
+// a) O que vai ser impresso no console?
+
+// b) Se eu quisesse acessar o índice de cada elemento dessa lista, a sintaxe "for...of..." é suficiente? Se sim, o que poderia ser usado para fazer isso?
