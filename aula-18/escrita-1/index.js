b107fb7a7338a6fc487fa1618efdf967238a02e3
jonas <jonas96@edu.unisinos.br>
2023-09-05 10:38:48 -0300

    [new] novos exercicios

---
+++ aula-18/escrita-1/index.js
@@ -0,0 +1,12 @@
+
+/**
+ * 
+ * Pergunte ao usuário quantos bichos de estimação ele tem e guarde esse dado em uma variável.
+ * 
+ * Se a quantidade for 0, imprima no console "Que pena! Você pode adotar um pet!".
+ * 
+ * Se a quantidade for maior que 0, solicite que o usuário digite os nomes deles, um por um, e guarde esses nomes em um array.
+ * Dica: coloque um "prompt" dentro de um loop. Esse loop deve ser executado a mesma quantidade de vezes que o usuário inseriu. Por exemplo: se o usuário tem 4 pets, ele deve conseguir inserir 4 nomes.
+ * Por fim, imprima no console o "array" completo, com os nomes dos bichos.
+ * 
+**/
---
+++ aula-18/escrita-2/index.js
@@ -0,0 +1,30 @@
+
+/**
+ * 
+ * Considere que você tenha acesso a um "array" que é composto somente de números. Crie uma função para cada um dos itens abaixo, realizando as operações pedidas:
+ * 
+ * 1) Escreva uma função que imprime cada valor do "array" em uma linha.
+ * 
+ * 2) Escreva uma função que imprime cada um dos valores do "array" divididos por 10.
+ * 
+ * 3) Escreva uma função que cria um novo array contendo somente os números pares do "array original" e, depois, imprima esse novo array.
+ * 
+ * 4) Escreva uma função que cria um novo array contendo strings no seguinte formato: "O elemento do índice 'i' é: 'numero'". Depois, imprima esse novo array.
+ * 
+ * 5) Escreva uma função que imprime no console o maior e o menor números contidos no "array original".
+ * 
+**/
+
+
+const arrayOriginal = [ 80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55 ]
+
+
+function imprimirLista (itens) {}
+
+function imprimirValoresDivididos (itens) {}
+
+function filtrarNumerosPares (numeros) {}
+
+function identificarElementos (itens) {}
+
+function imprimirMaiorMenor (itens) {}
